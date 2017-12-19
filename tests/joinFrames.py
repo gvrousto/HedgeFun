08caeacabcb3e3d3038b4a09eeca77bc8d864bc7
Gus Vroustouris <gvrousto@purdue.edu>
2017-12-19 02:54:07 -0600

    cleaned up joinFrames

--- tests/joinFrames.py
+++ tests/joinFrames.py
@@ -1,27 +1,26 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def symbol_to_path(symbol, base_dir="data"):
+	return os.path.join(base_dir, "{}.csv".format(str(symbol)))
+
 def test_run():
-	start_date = '2016-12-18'
+	start_date = '2017-7-18'
 	end_date = '2017-12-18'
 	dates = pd.date_range(start_date, end_date)
 	emptydf = pd.DataFrame(index=dates)
 
-	spydf = pd.read_csv('SPY.csv',index_col="Date",parse_dates=True,usecols=['Date','Adj Close'],na_values=['nan'])
+	#SPY always has to be first for reference
+	symbols = ['SPY','GOOG', 'AAPL']
 	
-
-	#rename title of column to prevent future clash
-	spydf = spydf.rename(columns = {'Adj Close' : 'SPY'})
-	
-
-
-	#only includes values for index that appear in left table
-	emptydf = emptydf.join(spydf, how='inner')
-	
-	symbols = ['GOOG', 'AAPL']
 	for symbol in symbols:
-		temp_df = pd.read_csv('{}.csv'.format(symbol),index_col="Date",parse_dates=True,usecols=['Date','Adj Close'],na_values=['nan'])
+		#create dataframe from csv
+		temp_df = pd.read_csv(symbol_to_path(symbol), index_col="Date", parse_dates=True,usecols=['Date','Adj Close'],na_values=['nan'])
+		
+		#rename to prevent clash
 		temp_df = temp_df.rename(columns = {'Adj Close' : symbol})
+		#left join to empty dataframe
 		emptydf = emptydf.join(temp_df, how='inner')
 
 	print emptydf
