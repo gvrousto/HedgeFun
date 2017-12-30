01d6ee56f70722873e986f04b76ef072e89741e6
Gus Vroustouris <gvrousto@purdue.edu>
2017-12-30 00:23:41 -0600

    modifying numpy elements

--- tests/numPyTest.py
+++ tests/numPyTest.py
@@ -65,5 +65,13 @@ def test_run():
 	#access elements
 	print a[3,2]
 	print a[0,1:3]
+
+	#modifying element
+	a[0,0] = 93
+	a[1,:] = 6
+	a[:,3] = [1,2,3,4,5]
+	print a
+
+
 if __name__ == "__main__":
 	test_run()
